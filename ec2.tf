0c795fbf9215caa1fd4a022c893e8c35e061ae64
muley2525 <122787182+muley2525@users.noreply.github.com>
2023-05-30 09:14:31 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,8 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIASCWTKEKHZ2OBVACX"
-  secret_key = "XoYoilgUwu21Qy72dRfxUS75SSmclWTCHAMB6zVy"
 }
 
-resource "aws_instance" "sam" {
+resource "aws_instance" "sample" {
     ami = "ami-0f5ee92e2d63afc18"
     instance_type = "t2.micro"
 }
